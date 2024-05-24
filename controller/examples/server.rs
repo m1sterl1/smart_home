6c605a6156e02fbef8df4a7d6192f38c1e0114cb
basil <basil@localhost.localdomain>
2024-05-24 15:46:27 +0300

    Removed controller

--- controller/examples/server.rs
+++
@@ -1,9 +0,0 @@
-use controller::server::Controller;
-
-fn main() {
-    // Create SocetServer
-    let server = Controller::new("127.0.0.1:10221").unwrap();
-    // Create (register) "First" socket on srever side
-    server.add_socket("First");
-    server.listen();
-}
