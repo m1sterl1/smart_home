8da3f82b7b9ecc2b0a53a2cb79429289065425ba
rustynull <>
2024-08-31 10:40:00 +0300

    Connection row

---
+++ gui/src/main.rs
@@ -0,0 +1,123 @@
+use std::borrow::BorrowMut;
+use std::default;
+use std::net::ToSocketAddrs;
+use std::sync::Arc;
+
+use iced::overlay::menu::Catalog;
+use iced::widget::{button, column, row, text, Column, Row, text_input, TextInput};
+use iced::{Center, Length, Settings, Task, Theme};
+
+use network::command::CommandRequest;
+use network::sync::{Client, TCPClient};
+use tokio::net::TcpSocket;
+use tokio::sync::{oneshot, mpsc};
+
+use network::r#async::{ClientAsync, TCPClientAsync};
+
+const CONNECTION_PLACEHODER: &str = "http://127.0.0.1:8080";
+const TEXT_INPUT_SIZE: f32 = 250.0;
+const BUTTON_SIZE: f32 = 100.0;
+const SOCKET_ID:&str = "id";
+
+pub fn main() -> iced::Result {
+    iced::application("Net Socket", SocketUI::update, SocketUI::view).run()
+}
+
+#[derive(Default)]
+struct SocketUI {
+    client: Option<TCPClient>,
+    state: SocketState,
+    power: u32,
+    status: String,
+    connection_string: String,
+}
+
+#[derive(Default)]
+enum SocketState{
+    #[default]
+    Disconnected,
+    On,
+    Off
+}
+
+
+#[derive(Clone, Debug)]
+enum Message {
+    Connect(String),
+    TurnOn,
+    TurnOff,
+    Power(u32),
+    InputChanged(String),
+}
+
+
+
+impl SocketUI{
+
+    fn update(&mut self, message: Message){
+        match message{
+            Message::Connect(a) => {
+                match TCPClient::new(a){
+                    Ok(client) => {
+                        self.client = Some(client);
+                        self.state = SocketState::On;
+                    },
+                    Err(e) => {
+                        self.status = e.to_string();
+                    }
+                }
+            },
+            Message::TurnOn => {
+                self.client.as_mut().map(|client|{
+                    match client.send(CommandRequest::builder().socket(SOCKET_ID).turn_on()){
+                        Ok(_) => self.state = SocketState::On,
+                        Err(e) => self.status = e.to_string(),
+                    }
+                });
+            },
+            Message::TurnOff => {
+                self.client.as_mut().map(|client|{
+                    match client.send(CommandRequest::builder().socket(SOCKET_ID).turn_off()){
+                        Ok(_) => {
+                            self.state = SocketState::Off;
+                            self.power = 0;
+                        },
+                        Err(e) => self.status = e.to_string(),
+                    }
+                });
+            },
+            Message::Power(p) => {
+                self.power = p;
+            },
+            Message::InputChanged(s) => {self.connection_string = s;}
+        }
+    }
+
+    
+    fn view(&self) -> Column<Message>{
+        column![
+            // connection row
+            self.connection_row(),
+            // control row
+            // status
+            text(&self.status)
+        ]
+    }
+
+    fn connection_row(&self) -> Row<Message>{
+        // text input
+        let text_input = text_input(CONNECTION_PLACEHODER, &self.connection_string)
+        .on_input(|input|Message::InputChanged(input))
+        .width(TEXT_INPUT_SIZE);
+        // submit button
+        let mut button = button("Connect");
+        // disable button when socket already connected
+        match self.state{
+            SocketState::Disconnected => {
+                button = button.on_press(Message::Connect(self.connection_string.clone()))
+            },
+            _ => {}
+        };         
+        row![text_input,button]
+    }
+}
